2c547b0d8556a1849a576f22b3fd6cbd65879a46
Viktor <avf1973@mail.ru>
2023-07-10 17:22:40 +0500

    задача 15

---
+++ Exs1_3/Program.cs
@@ -0,0 +1,36 @@
+﻿int Prompt (string message)
+{
+    Console. Write (message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+bool IsWeekend (int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekday(int number)
+{
+    if(number > 0 && number <= 7);
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+int weekDay = Prompt("Введите день недели >");
+if(ValidateWeekday(weekDay))
+
+    if(IsWeekend(weekDay))
+    {
+    Console.WriteLine("Этот день выходной");
+    }
+    else
+    {
+    Console.WriteLine("Рабочие будни");
+    }
