53fb94b7e29cd835c7982bfd20d574805d31b1a7
Viktor <avf1973@mail.ru>
2023-07-13 11:50:06 +0500

    задача 19

---
+++ Exs1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿ // Напишите программу, которая принимает на вход пятизначное
+ // число и проверяет, является ли оно палиндромом
+
+ Console.WriteLine("введите пятизначное число");
+     string x = Console.ReadLine();
+     char[] arr = x.ToCharArray();
+    Array.Reverse(arr);
+    string x2 = new String(arr);
+ 
+ if (x == x2) Console.WriteLine("Палиндром");
+         else Console.WriteLine("Не палиндром");
