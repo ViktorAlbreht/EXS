abdd8076d70603d604cc4f5adbe15788d90d6b73
Viktor <avf1973@mail.ru>
2023-07-13 12:38:16 +0500

    задача 23

---
+++ Exs1_6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход 
+//число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Таблица кубов:\n");
+    for (int i=1;i<=n;i++)
+{
+    int k = i*i*i;
+Console.Write($"{k}, ");
+}
